ea9f0cacc7d717fa7cd38a8df29883baabb82700
RealRadOne <shrishtisakshi311@gmail.com>
2025-12-29 04:36:45 -0600

    Adding find

---
+++ user/find.c
@@ -0,0 +1,100 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+	
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  buf[sizeof(buf)-1] = '\0';
+  return buf;
+}
+
+char* basename(char *path){
+	char *p;
+	for(p=path+strlen(path);p>=path && *p!='/';p--);
+	return p+1;
+}
+
+void find(char *path,char *target)
+{
+	char buf[512],*p;
+	int fd;
+	struct dirent de;
+	struct stat st;
+
+	if((fd=open(path,O_RDONLY))<0){
+		fprintf(2,"ls:cannot open %s\n",path);
+		return;
+	}
+
+	if(fstat(fd,&st)<0){
+		fprintf(2,"ls cannot stat %s\n",path);
+		close(fd);
+		return;
+	}
+
+	switch(st.type){
+  		case T_FILE:
+		if(strcmp(basename(path),target)==0){
+			printf("%s\n",path);
+			//exec("echo",path);
+		}
+    		break;
+
+  		case T_DIR:
+		if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      		printf("ls: path too long\n");
+      		break;
+    		}
+    		
+		strcpy(buf, path);
+    		p = buf+strlen(buf);
+    		*p++ = '/';
+    		
+		while(read(fd, &de, sizeof(de)) == sizeof(de)){
+			
+			if(de.inum == 0)
+        			continue;
+      			memmove(p, de.name, DIRSIZ);
+      			p[DIRSIZ] = 0;
+			if(strcmp(p,".")==0)
+				continue;
+			if(strcmp(p,"..")==0)
+				continue;
+			find(buf,target);
+		}
+    		break;
+  		}
+  		close(fd);	
+	}
+
+int main(int argc,char *argv[])
+{
+	if(argc<3)
+	{
+		printf("You need to provide a filename\n");
+		exit(0);
+	}
+	else
+	{
+		find(argv[1],argv[2]);
+		exit(0);
+	}
+}
